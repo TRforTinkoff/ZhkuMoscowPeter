b6ae675168f9f91fbc1bbb8b4a4a1af3056c9677
Tatiana Ryzhkova <tatiana.ryzhkova@outlook.com>
2018-02-10 22:13:32 +0300

    first commit

---
+++ src/main/java/core/MainPage.java
@@ -0,0 +1,162 @@
+package core;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainPage  {
+	
+	WebDriver driver; 
+    
+    public static final String MAINPAGE_URL="https://www.tinkoff.ru/";    
+    
+    public void setDriverGeco() {
+    	 System.setProperty("webdriver.gecko.driver", "/Users/tatianaryzhkova/Downloads/geckodriver2");
+//        DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+//        capabilities.setCapability("marionette", true);
+//        driver = new FirefoxDriver(capabilities);
+//   DesiredCapabilities capabilities=DesiredCapabilities.chrome();
+//        capabilities.setCapability("marionette", true);
+//      driver = new ChromeDriver(capabilities);
+        driver = new FirefoxDriver();
+        driver.get(MAINPAGE_URL);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        System.out.println("Gecko driver for method was setup"); }
+    
+        // constructor
+	public MainPage (WebDriver driver) {
+		this.driver = driver;  
+        // creating all WebElements
+        PageFactory.initElements(driver, this); } 
+	
+	
+	// Elements from Start Page
+	    @FindBy(tagName = "title")
+	    static WebElement pageTitle;
+	    
+	    @FindBy(xpath = "//span[contains(.,'Платежи')]")
+	    static WebElement paymentsLinkGlobal;
+	
+	
+	// Elements from Payment Page
+	@FindBy(xpath = "//li[2]/span[2]/a/span")
+	// @FindBy(xpath = "//div[1]/div/div/div/span/span/span")
+	// @FindBy(xpath = "//span[contains(.,'ЖКХ')]")
+	// @FindBy(xpath = "//path[@data-qa-file='Housing']")
+	@CacheLookup
+	static WebElement kommunalniePlatezhiLink;
+	
+	@FindBy(xpath = "//div[2]/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/span/span/span")
+	@CacheLookup
+	static WebElement regionLink;
+	
+	@FindBy(xpath = "//section")
+	// @FindBy(xpath = "//li[1]/span[2]/a/span")
+	// @FindBy(xpath = "//li[1]/span[2]/a/span/text()")
+	// @FindBy(xpath = "//span[contains(.,'ЖКУ-Москва')]")
+	@CacheLookup
+	static WebElement housingMoscowLinkGlobal;
+	
+	@FindBy(xpath = "//label/input")
+	@CacheLookup
+	static WebElement searchFieldINN;
+	
+	@FindBy(xpath = "//div[1]/input")
+	@CacheLookup
+	static WebElement regionInputFieldGlobal;
+	
+	@FindBy(xpath = "//form/div[3]/div/div/div/div/div/div")
+	@CacheLookup
+	static WebElement regionInputSearchLink;
+	
+	
+	@FindBy(xpath = "//div[2]/div[2]/div/span/a/span")
+	static WebElement regionStPetersburg;
+	 
+	
+	// Elements from HousingMoscow Page
+	@FindBy(xpath = "//div[3]/div/ul/li[2]/div/a/span")
+	static WebElement housingMoscowLink;
+	
+	@FindBy(xpath = "id('payerCode')")
+	//@FindBy(id = "payerCode")
+	static WebElement payerCodeField;
+	
+	@FindBy(xpath = "//form/div[2]/div/div[1]/label/div[1]/input")
+	static WebElement emailField;
+	
+	@FindBy(xpath = "//form/div[1]/div[1]/div/div/div/label/div[1]/input")
+	static WebElement loginOrPhoneField;
+	
+	@FindBy(xpath = "//div[4]/div[1]/div[1]/div/button")
+	static WebElement knowDebtButton;
+	
+	@FindBy(xpath = "//form/div[1]/div/div[2]")
+	static WebElement zhkuFieldEM;
+	
+	@FindBy(xpath = "//form/div[1]/div[1]/div/div/div[2]")
+	static WebElement loginFieldEM;
+	
+	@FindBy(xpath = "//*[@id='mainMenu']/li[3]/span/a/span")
+    static WebElement paymentsLinkHousingPage;
+	 
+	
+      //methods
+	
+	 public String getPageTitle(){
+        return    pageTitle.getText(); }
+    
+     public void navigateToMainPage() {
+        driver.navigate().to(MAINPAGE_URL); }
+    
+	 public void paymentLinkClick() {
+		  paymentsLinkGlobal.click(); }
+
+     public void clickPaymentsLink() {
+       if(paymentsLinkGlobal.isDisplayed()) {
+    	   paymentsLinkGlobal.click(); }
+       getPageTitle();
+        System.out.println(getPageTitle().toString());  }  
+   
+     public void kommunalniePlatezhiLinkClick() {
+    	 kommunalniePlatezhiLink.click(); }
+   
+     public String getRegion() {
+    	return regionLink.getText();
+    }
+     
+     public void clickZhkuMoscowLink() {
+        housingMoscowLinkGlobal.click();     }
+     
+     public void payerCodePresent() {
+    	 payerCodeField.isDisplayed(); }
+
+     public void changeRegionMscStPet() {
+    	 regionLink.click();
+    	 regionStPetersburg.click();   }
+     
+     
+	  public void enterStPetersburgRegion(String regionStPetersburg) {
+		  regionInputFieldGlobal.sendKeys(regionStPetersburg);
+		 // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  regionInputSearchLink.click(); }
+	  
+	  public void enterInvalidDataEH(String payerCodeInvalid) {
+		  payerCodeField.sendKeys(payerCodeInvalid);
+		  knowDebtButton.click(); }
+
+      public String getZhkuFieldErrorMessage() {
+          return zhkuFieldEM.getText(); }
+      
+      public String getLoginFieldErrorMessage() {
+          return loginFieldEM.getText(); }
+
+//end of Class
+	} 
---
+++ src/test/java/core/MainPageTest.java
@@ -0,0 +1,89 @@
+package core;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+public class MainPageTest {
+	WebDriver driver;
+	
+	//to be transferred to csv file
+	private static String startPageTitle = "Лучший интернет-банк. Кредит наличными и кредитные карты онлайн";
+	private static String paymentPageTitle = "Tinkoff.ru: платежи и переводы денег";
+	private static String providersPageTitle = "Оплата ЖКХ без комиссии. Коммунальные платежи онлайн";
+	private static String zhkuMoscowPageTitle = "ЖКУ-Москва | Онлайн-проверка задолженностей ЖКУ-Москва";
+	private static String cityMoscow = "Москве";
+	private static String cityStPetersburg = "Санкт-Петербурге";
+	private static String payerCodeInvalid = "123";
+	private static String regionStPetersburg = "Санкт-Петербург";
+
+	
+//        	@BeforeMethod(alwaysRun = true)
+//	    public void driverSetupGecko() {
+//		 MainPage onMainPage = new MainPage(driver);
+//	    onMainPage.setDriverGeco();
+//        System.out.println("Gecko driver was setup"); }
+//	
+	@BeforeMethod(alwaysRun = true)
+	public void driverSetUp() {
+//	 	 
+        //DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        //capabilities.setCapability("marionette", true);
+	 	//WebDriver dr = new FirefoxDriver(capabilities);
+//	 	 
+	 	 System.setProperty("webdriver.gecko.driver", "/Users/tatianaryzhkova/Downloads/geckodriver2"); 	 
+	 	WebDriver dr = new FirefoxDriver();
+	 	dr.get("https://www.tinkoff.ru");	 }
+	 
+	
+	@Test
+	public void verifyStartPageTitle() {
+		 MainPage onMainPage = new MainPage(driver);
+		 onMainPage.navigateToMainPage();
+		 onMainPage.getPageTitle().toString().contains(startPageTitle); }
+
+ 	//пункт 1-6
+    @Test
+    public void payZhkuMoscow() {
+	    MainPage onMainPage = new MainPage(driver);
+	    onMainPage.navigateToMainPage();
+	    onMainPage.paymentLinkClick();
+	    onMainPage.kommunalniePlatezhiLinkClick();
+	    AssertJUnit.assertTrue(onMainPage.getRegion().toString().contains(cityMoscow));
+	    onMainPage.clickZhkuMoscowLink();
+	    onMainPage.payerCodePresent(); }
+    
+  //пункт 7
+    @Test
+    public void payerCodeEH() {
+	    MainPage onMainPage = new MainPage(driver);
+	    onMainPage.navigateToMainPage();
+	    onMainPage.paymentLinkClick();
+	    onMainPage.kommunalniePlatezhiLinkClick();
+	    onMainPage.clickZhkuMoscowLink();
+	    onMainPage.enterInvalidDataEH(payerCodeInvalid);
+	    AssertJUnit.assertTrue(onMainPage.getRegion().toString().contains(cityMoscow)); }
+    
+  //пункт 13
+    @Test
+    public void changeRegionStPetersburg() {
+	    MainPage onMainPage = new MainPage(driver);
+	    onMainPage.navigateToMainPage();
+	    onMainPage.paymentLinkClick();
+	    onMainPage.kommunalniePlatezhiLinkClick();
+	    onMainPage.enterStPetersburgRegion(regionStPetersburg);    }
+
+ 
+    @AfterTest(alwaysRun = true)
+    public void closeWebdriver() {
+ 	   if(driver != null) {
+ 	   driver.quit(); } }
+    
+    //end of Class
+     }
